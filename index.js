18cb58c3b3cad3c46c71a94c0e04981941f3d85d
sociablerobot10 <106482005+sociablerobot10@users.noreply.github.com>
2023-03-06 17:16:27 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+import { menuArray } from './data.js'
+
+
+let menu = document.getElementById('menu')
+let ingred = document.getElementById('ingredients')
+let html =''
+
+for(let item of menuArray) {
+    console.log(item)
+    
+    html += `<section id="item"><span id="left-side-emoji">
+                    ${item.emoji}
+                </span>
+                 <section id="right-side">
+                <span id="item-name">
+                    ${item.name}</span>
+                <div id="ingredients">`
+    
+    for(let food of item.ingredients){
+         
+      /*  if(item.ingredients.length-1 != item.ingredients.indexOf(food)){
+        menu.innerHTML += ` ${food}, `
+        }
+        
+        
+        else {
+         menu.innerHTML += ` ${food}</div></section></section>`
+        
+        }*/
+        
+      
+       html += `${food}, `
+        }
+          html += `</div>`
+         html += `<section class="price">$${item.price}</section></section></section>`
+    }
+    
+   menu.innerHTML += html;
+    
+    
+console.log(menu.innerHTML)
+
+
+
+
+
